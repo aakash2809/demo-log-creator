ed12241f4f4c62ac8b97ef078498381fa3f50a07
aakash2809 <aakashrajak289@gmail.com>
2021-01-01 14:10:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,148 @@
+'use strict';
+const { createLogger, format, transports } = require('winston');
+
+/* const logger = createLogger({
+  level: 'debug',
+  format: format.simple(),
+  transports: [new transports.Console()]
+}); */
+
+/* const logger = createLogger({
+    level: 'silly',
+    format: format.simple(),
+    transports: [new transports.Console()]
+  });
+ */
+
+/* const logger = createLogger({
+    level: 'debug',
+    format: format.combine(
+      format.colorize(),
+      format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+      }),
+      format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
+    transports: [new transports.Console()]
+  });
+ */
+
+/* const fs = require('fs');
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const filename = path.join(logDir, 'results.log');
+
+const logger = createLogger({
+  // change level if in dev environment versus production
+  level: env === 'development' ? 'debug' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    }),
+    new transports.File({ filename })
+  ]
+});
+ */
+
+/* const fs = require('fs');
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const filename = path.join(logDir, 'results.log');
+
+const logger = createLogger({
+  // change level if in dev environment versus production
+  level: env === 'development' ? 'debug' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.json()
+  ),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    }),
+    new transports.File({ filename })
+  ]
+}); */
+
+require('winston-daily-rotate-file');
+const fs = require('fs');
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const dailyRotateFileTransport = new transports.DailyRotateFile({
+  filename: `${logDir}/%DATE%-results.log`,
+  datePattern: 'YYYY-MM-DD'
+});
+
+const logger = createLogger({
+  // change level if in dev environment versus production
+  level: env === 'development' ? 'verbose' : 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    }),
+    dailyRotateFileTransport
+  ]
+});
+
+logger.info('Hello world');
+logger.debug('Debugging info');
+logger.verbose('Verbose info');
+//logger.silly('Very verbose silly message');
+logger.warn('Warning message');
+logger.error('Error info');
